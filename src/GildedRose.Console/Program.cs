c9def88704cd2dce05a3ddef68efe2ec911bc241
Phil Chuang <dev@philchuang.com>
2013-06-20 02:08:00 -0400

    refactored app from program

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -4,37 +4,46 @@ namespace GildedRose.Console
 {
     class Program
     {
-        IList<Item> Items;
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
-
-                          };
+			var app = App.CreateDefaultInstance();
 
             app.UpdateQuality();
 
             System.Console.ReadKey();
 
         }
+	}
 
-        public void UpdateQuality()
+	public class App
+	{
+		public IList<Item> Items;
+
+		public static App CreateDefaultInstance ()
+		{
+			return new App ()
+				       {
+					       Items = new List<Item>
+						               {
+							               new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+							               new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+							               new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+							               new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+							               new Item
+								               {
+									               Name = "Backstage passes to a TAFKAL80ETC concert",
+									               SellIn = 15,
+									               Quality = 20
+								               },
+							               new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+						               }
+
+				       };
+		}
+		
+		public void UpdateQuality ()
         {
             for (var i = 0; i < Items.Count; i++)
             {
