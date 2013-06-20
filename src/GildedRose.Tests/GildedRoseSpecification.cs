4af772a45d243a32b29c0d56d0a9e99e4ef07141
Phil Chuang <dev@philchuang.com>
2013-06-20 02:25:09 -0400

    added tests to prove normal item rules

--- src/GildedRose.Tests/GildedRoseSpecification.cs
+++ src/GildedRose.Tests/GildedRoseSpecification.cs
@@ -11,7 +11,7 @@ namespace GildedRose.Tests
 {
 	public abstract class GildedRoseSpecification : SpecBase
 	{
-		protected GildedRose.Console.App m_App;
+		protected App m_App;
 		protected Exception m_Because_of_Exception;
 
 		protected override void Establish_context ()
@@ -39,9 +39,12 @@ namespace GildedRose.Tests
 
 	public abstract class when_UpdateQuality_is_called : GildedRoseSpecification
 	{
+		protected Item m_Item;
+		protected int m_InitialItemQuality;
+		protected int m_InitialItemSellIn;
 		protected int m_NumTimesCalled = 1;
 
-		protected override void Establish_context ()
+		protected override void Because_of ()
 		{
 			try
 			{
@@ -52,6 +55,90 @@ namespace GildedRose.Tests
 			{
 				m_Because_of_Exception = ex;
 			}
+		}
+
+		[Test]
+		public void then_SellIn_should_decrease ()
+		{
+			Assert.AreEqual (m_InitialItemSellIn - m_NumTimesCalled, m_Item.SellIn);
+		}
+	}
+
+	public class when_UpdateQuality_is_called_with_normal_item_with_nonzero_Quality_and_positive_SellIn : when_UpdateQuality_is_called
+	{
+		protected override void Establish_context ()
+		{
+			base.Establish_context ();
+
+			m_InitialItemQuality = 10;
+			m_InitialItemSellIn = 10;
+			m_Item = new Item
+				               {
+					               Name = "Normal item",
+								   Quality = m_InitialItemQuality,
+								   SellIn = m_InitialItemSellIn,
+				               };
+
+			m_App.Items.Clear();
+			m_App.Items.Add (m_Item);
+		}
+
+		[Test]
+		public void then_Quality_should_decrease_by_1 ()
+		{
+			Assert.AreEqual (m_InitialItemQuality - 1, m_Item.Quality);
+		}
+	}
+
+	public class when_UpdateQuality_is_called_with_normal_item_with_zero_Quality_and_positive_SellIn : when_UpdateQuality_is_called
+	{
+		protected override void Establish_context ()
+		{
+			base.Establish_context ();
+
+			m_InitialItemQuality = 0;
+			m_InitialItemSellIn = 10;
+			m_Item = new Item
+				               {
+					               Name = "Normal item",
+								   Quality = m_InitialItemQuality,
+								   SellIn = m_InitialItemSellIn,
+				               };
+
+			m_App.Items.Clear();
+			m_App.Items.Add (m_Item);
+		}
+
+		[Test]
+		public void then_Quality_should_equal_0 ()
+		{
+			Assert.AreEqual (0, m_Item.Quality);
+		}
+	}
+
+	public class when_UpdateQuality_is_called_with_normal_item_with_nonzero_Quality_and_zero_SellIn : when_UpdateQuality_is_called
+	{
+		protected override void Establish_context ()
+		{
+			base.Establish_context ();
+
+			m_InitialItemQuality = 10;
+			m_InitialItemSellIn = 0;
+			m_Item = new Item
+				               {
+					               Name = "Normal item",
+								   Quality = m_InitialItemQuality,
+								   SellIn = m_InitialItemSellIn,
+				               };
+
+			m_App.Items.Clear();
+			m_App.Items.Add (m_Item);
+		}
+
+		[Test]
+		public void then_Quality_should_decrease_by_2 ()
+		{
+			Assert.AreEqual (m_InitialItemQuality - 2, m_Item.Quality);
 		}
 	}
 }
