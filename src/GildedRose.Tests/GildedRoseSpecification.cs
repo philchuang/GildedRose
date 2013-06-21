38b057f00748e081d366ac4eb6c460d6871dc423
Phil Chuang <dev@philchuang.com>
2013-06-21 01:34:49 -0400

    refactored tests to reduce duplication
    also added test for Aged Brie

--- src/GildedRose.Tests/GildedRoseSpecification.cs
+++ src/GildedRose.Tests/GildedRoseSpecification.cs
@@ -64,23 +64,32 @@ namespace GildedRose.Tests
 		}
 	}
 
-	public class when_UpdateQuality_is_called_with_normal_item_with_nonzero_Quality_and_positive_SellIn : when_UpdateQuality_is_called
+	public abstract class when_UpdateQuality_is_called_with_normal_item : when_UpdateQuality_is_called
 	{
 		protected override void Establish_context ()
 		{
 			base.Establish_context ();
 
-			m_InitialItemQuality = 10;
-			m_InitialItemSellIn = 10;
 			m_Item = new Item
-				               {
-					               Name = "Normal item",
-								   Quality = m_InitialItemQuality,
-								   SellIn = m_InitialItemSellIn,
-				               };
+				         {
+					         Name = "Normal item",
+					         Quality = m_InitialItemQuality,
+					         SellIn = m_InitialItemSellIn,
+				         };
 
 			m_App.Items.Clear();
 			m_App.Items.Add (m_Item);
+		}
+	}
+
+	public class when_UpdateQuality_is_called_with_normal_item_with_nonzero_Quality_and_positive_SellIn : when_UpdateQuality_is_called_with_normal_item
+	{
+		protected override void Establish_context ()
+		{
+			m_InitialItemQuality = 10;
+			m_InitialItemSellIn = 10;
+
+			base.Establish_context ();
 		}
 
 		[Test]
@@ -90,23 +99,14 @@ namespace GildedRose.Tests
 		}
 	}
 
-	public class when_UpdateQuality_is_called_with_normal_item_with_0_Quality : when_UpdateQuality_is_called
+	public class when_UpdateQuality_is_called_with_normal_item_with_0_Quality : when_UpdateQuality_is_called_with_normal_item
 	{
 		protected override void Establish_context ()
 		{
-			base.Establish_context ();
-
 			m_InitialItemQuality = 0;
 			m_InitialItemSellIn = 10;
-			m_Item = new Item
-				               {
-					               Name = "Normal item",
-								   Quality = m_InitialItemQuality,
-								   SellIn = m_InitialItemSellIn,
-				               };
 
-			m_App.Items.Clear();
-			m_App.Items.Add (m_Item);
+			base.Establish_context ();
 		}
 
 		[Test]
@@ -116,23 +116,14 @@ namespace GildedRose.Tests
 		}
 	}
 
-	public class when_UpdateQuality_is_called_with_normal_item_with_nonzero_Quality_and_0_SellIn : when_UpdateQuality_is_called
+	public class when_UpdateQuality_is_called_with_normal_item_with_nonzero_Quality_and_0_SellIn : when_UpdateQuality_is_called_with_normal_item
 	{
 		protected override void Establish_context ()
 		{
-			base.Establish_context ();
-
 			m_InitialItemQuality = 10;
 			m_InitialItemSellIn = 0;
-			m_Item = new Item
-				               {
-					               Name = "Normal item",
-								   Quality = m_InitialItemQuality,
-								   SellIn = m_InitialItemSellIn,
-				               };
 
-			m_App.Items.Clear();
-			m_App.Items.Add (m_Item);
+			base.Establish_context ();
 		}
 
 		[Test]
@@ -142,14 +133,12 @@ namespace GildedRose.Tests
 		}
 	}
 
-	public class when_UpdateQuality_is_called_with_AgedBrie_with_0_Quality_and_positive_SellIn : when_UpdateQuality_is_called
+	public abstract class when_UpdateQuality_is_called_with_AgedBrie : when_UpdateQuality_is_called
 	{
 		protected override void Establish_context ()
 		{
 			base.Establish_context ();
 
-			m_InitialItemQuality = 0;
-			m_InitialItemSellIn = 1;
 			m_Item = new Item
 				               {
 					               Name = "Aged Brie",
@@ -159,6 +148,17 @@ namespace GildedRose.Tests
 
 			m_App.Items.Clear();
 			m_App.Items.Add (m_Item);
+		}
+	}
+
+	public class when_UpdateQuality_is_called_with_AgedBrie_with_0_Quality_and_positive_SellIn : when_UpdateQuality_is_called_with_AgedBrie
+	{
+		protected override void Establish_context ()
+		{
+			m_InitialItemQuality = 0;
+			m_InitialItemSellIn = 1;
+
+			base.Establish_context ();
 		}
 
 		[Test]
@@ -168,23 +168,14 @@ namespace GildedRose.Tests
 		}
 	}
 
-	public class when_UpdateQuality_is_called_with_AgedBrie_with_50_Quality : when_UpdateQuality_is_called
+	public class when_UpdateQuality_is_called_with_AgedBrie_with_50_Quality : when_UpdateQuality_is_called_with_AgedBrie
 	{
 		protected override void Establish_context ()
 		{
-			base.Establish_context ();
-
 			m_InitialItemQuality = 50;
 			m_InitialItemSellIn = 0;
-			m_Item = new Item
-				               {
-					               Name = "Aged Brie",
-								   Quality = m_InitialItemQuality,
-								   SellIn = m_InitialItemSellIn,
-				               };
 
-			m_App.Items.Clear();
-			m_App.Items.Add (m_Item);
+			base.Establish_context ();
 		}
 
 		[Test]
@@ -194,23 +185,14 @@ namespace GildedRose.Tests
 		}
 	}
 
-	public class when_UpdateQuality_is_called_with_AgedBrie_with_0_SellIn : when_UpdateQuality_is_called
+	public class when_UpdateQuality_is_called_with_AgedBrie_with_lessthan_48_quality_and_0_SellIn : when_UpdateQuality_is_called_with_AgedBrie
 	{
 		protected override void Establish_context ()
 		{
-			base.Establish_context ();
-
 			m_InitialItemQuality = 0;
 			m_InitialItemSellIn = 0;
-			m_Item = new Item
-			{
-				Name = "Aged Brie",
-				Quality = m_InitialItemQuality,
-				SellIn = m_InitialItemSellIn,
-			};
 
-			m_App.Items.Clear ();
-			m_App.Items.Add (m_Item);
+			base.Establish_context ();
 		}
 
 		/// <summary>
@@ -220,6 +202,26 @@ namespace GildedRose.Tests
 		public void then_Quality_should_increase_by_2 ()
 		{
 			Assert.AreEqual (m_InitialItemQuality + 2, m_Item.Quality);
+		}
+	}
+
+	public class when_UpdateQuality_is_called_with_AgedBrie_with_than_49_quality_and_0_SellIn : when_UpdateQuality_is_called_with_AgedBrie
+	{
+		protected override void Establish_context ()
+		{
+			m_InitialItemQuality = 49;
+			m_InitialItemSellIn = 0;
+
+			base.Establish_context ();
+		}
+
+		/// <summary>
+		/// This rule was not explicitly stated, but found via testing
+		/// </summary>
+		[Test]
+		public void then_Quality_should_be_50 ()
+		{
+			Assert.AreEqual (50, m_Item.Quality);
 		}
 	}
 }
